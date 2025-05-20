945b0358b2faa0ecaaffbb352ec3ed606a31d5cb
Gustavo Zesczylinski <germangz@rocketmail.com>
2025-05-20 14:19:39 -0300

    Implementing sockets

---
+++ public/socket-front-doc.js
@@ -0,0 +1,18 @@
+import { atualizarTextoEditor } from "./documento.js";
+
+const socket = io();
+
+function emitirTextoEditor(texto) {
+    socket.emit('texto_editor', texto);
+}
+
+socket.on('texto_editor_clientes', (texto) => {
+    atualizarTextoEditor(texto);
+});
+
+socket.on("disconnect", (motivo) => {
+  console.log(`Servidor desconectado!
+  Motivo: ${motivo}`);
+});
+
+export { emitirTextoEditor };
